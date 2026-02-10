061e3876a96d94a92681c1ee5140af37126deb85
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-10 16:19:03 +0100

    Inicialización del proyecto Spring Boot 3.5.10 con las depencias necesarias

---
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/MireservafitApplication.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.mireservafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireservafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MireservafitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ifc33b/dwesc/mireservafit/MireservafitApplicationTests.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.mireservafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireservafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
