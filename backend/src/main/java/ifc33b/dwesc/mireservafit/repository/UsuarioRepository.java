7c30e8fe5f5635244e7d09e72c6296d34a080273
espinozam <edwin.smx2@gmail.com>
2026-02-15 16:59:48 +0100

    Añadido el método findByEmail en UsuarioRepository y devuelve Optional<Usuario> en caso de ausencia de usuario

--- backend/src/main/java/ifc33b/dwesc/mireservafit/repository/UsuarioRepository.java
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/repository/UsuarioRepository.java
@@ -5,7 +5,10 @@ import ifc33b.dwesc.mireservafit.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+    Optional<Usuario> findByEmail(String email);
 }
