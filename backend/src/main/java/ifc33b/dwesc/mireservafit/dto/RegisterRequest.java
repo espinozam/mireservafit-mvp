893e2d06f3b0469917463b3fcc2985d0df0593cf
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-16 19:54:14 +0100

    Actualizado DTO RegisterRequest, ahora tiene campos "fechaNacimiento" y "especialidad"

--- backend/src/main/java/ifc33b/dwesc/mireservafit/dto/RegisterRequest.java
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/dto/RegisterRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import java.time.LocalDate;
 
 @Data
 public class RegisterRequest {
@@ -23,4 +24,10 @@ public class RegisterRequest {
 
     @NotBlank(message = "Rol es obligatorio")
     private String rol;
+
+    // Opción para CLIENTE
+    private LocalDate fechaNacimiento;
+
+    // Opción para ENTRENADOR
+    private String especialidad;
 }
