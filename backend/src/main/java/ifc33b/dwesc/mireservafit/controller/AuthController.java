807f63a941fd0b062b239360318c8e146095a83b
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-16 19:49:56 +0100

    Creado controlador UsuarioController con Enpoints para login/register

---
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/controller/AuthController.java
@@ -0,0 +1,40 @@
+package ifc33b.dwesc.mireservafit.controller;
+
+import ifc33b.dwesc.mireservafit.model.Usuario;
+import ifc33b.dwesc.mireservafit.dto.LoginRequest;
+import ifc33b.dwesc.mireservafit.dto.RegisterRequest;
+import ifc33b.dwesc.mireservafit.dto.UsuarioResponse;
+import ifc33b.dwesc.mireservafit.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    // usar el servicio de autenticacion
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioResponse> login(@Valid @RequestBody LoginRequest request) {
+        try {
+            UsuarioResponse response = authService.login(request);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UsuarioResponse> register(@Valid @RequestBody RegisterRequest request) {
+        try {
+            UsuarioResponse response = authService.register(request);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
