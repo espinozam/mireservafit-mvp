a587d50acd97b1daa639c756bf37c07052914b24
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-17 16:23:25 +0100

    Añadido componente Login a routes y configurado como página por defecto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
   { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: '**', redirectTo: 'login' }
 ];
