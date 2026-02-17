a8c15a2f86a6ad08d7011cfb8d8f1968b7f36d25
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-17 16:40:02 +0100

    Añadido formulario y logica al componente Login

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  // atributos
+  email = '';
+  password = '';
+
+  // constructor
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  // metodo para hacer login
+  login() {
+    this.authService.login({
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: () =>  {
+          alert('Inicio de sesión correcto');
+        },
+      error: () => alert('Login incorrecto')
+    });
+  }
 }
