63faec7bde6df5305e1f3b6836303a3bfc65fd05
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-17 14:41:48 +0100

    Añadido lógica a AuthService para consumir API REST de registro y login

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+
+  // direccion a la api del backend
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  // constructor
+  constructor(private http: HttpClient) {}
+
+  // metodo para hacer login (crea HttpSession en el backend)
+  login(credenciales: any) {
+    return this.http.post<any>(
+      `${this.apiUrl}/login`,
+      credenciales,
+      { withCredentials: true } // enviar cookie de session
+    );
+  }
+
+  // metodo para registrar usuario
+  register(datos: any) {
+    return this.http.post<any>(
+      `${this.apiUrl}/register`,
+      datos,
+      { withCredentials: true } // backend inicia sesión tras registrar
+    );
+  }
 }
