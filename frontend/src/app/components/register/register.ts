6b6d2e9eae1fad80038593731cc936a4419226b0
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-17 14:20:24 +0100

    Añadido formulario para registro de usuarios cliente/entrenador

--- frontend/src/app/components/register/register.ts
+++ frontend/src/app/components/register/register.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
 
+  // atributos
+  nombre = '';
+  email = '';
+  password = '';
+  rol = "CLIENTE";
+
+  fechaNacimiento = '';
+  especialidad = '';
+
+  // constructor
+  constructor() {}
+
+  // metodo para registrar
+  registrar() {}
 }
+
