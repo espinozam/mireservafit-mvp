8dfff4b751752b80da56d333204e48389643d545
espinozam <edwin.smx2@gmail.com>
2026-02-19 10:03:52 +0100

    Añadida validación para que la fecha de reserva no sea pasada en DTO ReservaRequest

--- backend/src/main/java/ifc33b/dwesc/mireservafit/dto/ReservaRequest.java
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/dto/ReservaRequest.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.FutureOrPresent;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -17,6 +18,7 @@ public class ReservaRequest {
     private Integer idEntrenador;
 
     @NotNull(message = "Fecha de reserva es obligatorio")
+    @FutureOrPresent(message = "La fecha de reserva no puede ser en el pasado")
     private LocalDate fechaReserva;
 
     @NotNull(message = "Hora de inicio es obligatoria")
