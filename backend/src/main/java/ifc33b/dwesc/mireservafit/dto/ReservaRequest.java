cf9cfac494e5aaea3b2ea945432203f9db5ed8db
espinozam <edwin.smx2@gmail.com>
2026-02-19 09:58:28 +0100

    Creado DTO ReservaRequest

---
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/dto/ReservaRequest.java
@@ -0,0 +1,27 @@
+package ifc33b.dwesc.mireservafit.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservaRequest {
+    // atributos
+    @NotNull(message = "Entrenador es obligatorio")
+    private Integer idEntrenador;
+
+    @NotNull(message = "Fecha de reserva es obligatorio")
+    private LocalDate fechaReserva;
+
+    @NotNull(message = "Hora de inicio es obligatoria")
+    private LocalTime horaInicio;
+
+    @NotNull(message = "Hora de fin es obligatoria")
+    private LocalTime horaFin;
+}
