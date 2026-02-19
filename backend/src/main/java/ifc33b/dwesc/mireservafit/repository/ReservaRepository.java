4783100a70272feb3ef013f468e72ad1209fa6e4
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-19 21:00:56 +0100

    Añadido método personalizado findByEntrenadorIdAndFechaReserva() en ReservaRepository() para encontrar reservas por entrenador y fecha

--- backend/src/main/java/ifc33b/dwesc/mireservafit/repository/ReservaRepository.java
+++ backend/src/main/java/ifc33b/dwesc/mireservafit/repository/ReservaRepository.java
@@ -5,7 +5,11 @@ import ifc33b.dwesc.mireservafit.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
+    // Metodo para encontrar reservas por entrenador y fecha
+    List<Reserva> findByEntrenadorIdAndFechaReserva(Integer entrenadorId, LocalDate fechaReserva);
 }
