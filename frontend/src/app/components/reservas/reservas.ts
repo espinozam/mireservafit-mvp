970846db5a5766e71c9812e7e53ee8bd6dc5df78
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-21 20:02:19 +0100

    añadido imports y variable para formulario de datos en reservas.ts

--- frontend/src/app/components/reservas/reservas.ts
+++ frontend/src/app/components/reservas/reservas.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReservaService } from '../../services/reserva.service';
 
 @Component({
   selector: 'app-reservas',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './reservas.html',
   styleUrl: './reservas.scss',
 })
 export class Reservas {
+
+  // datos del formulario
+  reserva = {
+    idEntrenador: '',
+    fechaReserva: '',
+    horaInicio: ''
+  }
 
 }
