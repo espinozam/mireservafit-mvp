c1061297ebbe6830e918c4e59412a401a241e3af
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-21 18:56:01 +0100

    Actualizado modelo ReservaResponse con atributos/campos de la reserva realizada

--- frontend/src/app/models/reserva-response.model.ts
+++ frontend/src/app/models/reserva-response.model.ts
@@ -1,2 +1,9 @@
 export interface ReservaResponse {
+  id: number;
+  nombreCliente: string;
+  nombreEntrenador: string;
+  fechaReserva: string;
+  horaInicio: string;
+  horaFin: string;
+  estado: string;
 }
