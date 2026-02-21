d96350347e05220b1f75599b1ad6443b164909b7
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-21 21:03:16 +0100

    Añadido componente Reservas a app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
 import { Login } from './components/login/login';
+import { Reservas } from './components/reservas/reservas';
 
 export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'login', component: Login },
+  { path: 'reservas', component: Reservas },
   { path: '**', redirectTo: 'login' }
 ];
